16fc90c108c57e313a9019e3ecf2e6c6370f4dd1
carolhate23-commits <carolhate23@gmail.com>
2026-04-23 10:05:34 +0800

    Update 4_📬Contact.py

--- Portfolio/pages/4_📬Contact.py
+++ Portfolio/pages/4_📬Contact.py
@@ -3,14 +3,16 @@ import streamlit as st
 st.markdown("""
 <style>
 [data-testid="stSidebar"] {
-    background-color: green;
+    background-color:pink;
 }
 [data-testid="stSidebar"] * {
-    color: white;
+    color: black;
 }
+
 .stApp {
-   background-color: aliceblue;
-}   
+   background-color: lightblue;
+}  
+
 .main {
     text-align: center;
 }
@@ -34,17 +36,18 @@ img {
 </style>
 """, unsafe_allow_html=True)
 
-st.title(" 📬 :orange[Contact Me]")
+
+
+st.title("📬:green[Contact Me]")
 
 name = st.text_input("Name")
 email = st.text_input("Email")
 message = st.text_area("Message")
 if st.button("Send"):
-    if name and email and message:
-       st.success("Message sent successfully! ✅")
-    else:
-        st.error("Please fill all fields.")
-
-st.markdown("### 🌐Social Links")
-st.write("- GitHub: https://github.com/carenhatte-creator")
-st.write("- Facebook: https://facebook.com/caren.ha.te")
+ if name and email and message:
+      st.success("Message sent successfully! ✅")
+else:
+      st.error("Please fill all fields.")
+st.markdown("###🌐Social Links")
+st.write("- GitHub: https://github.com/carolhate@23-commits")
+st.write("- Facebook: https://facebook.com/carol.ha.te")
